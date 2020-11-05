0885bf9686e8a6501dfd6fe770be259db1b1f11e
Ali Raza <artanha7@gmail.com>
2020-11-05 16:21:34 +0500

    Add basic style to create component

--- src/app/admin/posts/create/create.component.ts
+++ src/app/admin/posts/create/create.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  selector: "app-create",
+  templateUrl: "./create.component.html",
+  styleUrls: ["./create.component.scss"],
 })
 export class CreateComponent implements OnInit {
-
-  constructor() { }
-
-
-  ngOnInit() {
+  constructor() {
+    this.postForm.valueChanges.subscribe(data => console.log(data));
   }
 
+
+  postForm = new FormGroup({
+    postTitle: new FormControl('', Validators.required),
+    postText: new FormControl('', Validators.required)
+  });
+
+
+  ngOnInit() {}
 }

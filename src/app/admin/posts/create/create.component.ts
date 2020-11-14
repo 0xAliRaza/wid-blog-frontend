e0f62a37e0c28f23ac259f968b86e058a95824fe
Ali Raza <artanha7@gmail.com>
2020-11-14 15:11:43 +0500

    Add tinyMce init to component directive

--- src/app/admin/posts/create/create.component.ts
+++ src/app/admin/posts/create/create.component.ts
@@ -39,9 +39,22 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
 export class CreateComponent implements OnInit {
   timeout: any;
   isSideNavOpen = false;
-
+  tinymceInit: any;
   constructor() {
     this.postForm.valueChanges.subscribe((data) => this.scheduleSave(data));
+    this.tinymceInit = {
+      menubar: false,
+      toolbar: false,
+      placeholder: "Write it all down...",
+      plugins:
+        "autoresize quickbars image media hr codesample code autolink image",
+      quickbars_selection_toolbar: "bold italic link | h2 h3 | blockquote",
+      quickbars_insert_toolbar: "image media hr codesample code",
+      statusbar: false,
+      image_advtab: true,
+      block_unsupported_drop: false,
+      images_upload_url: 'http://wid-blog-backend/api/v1/image'
+    };
   }
 
   postForm = new FormGroup({
@@ -53,9 +66,54 @@ export class CreateComponent implements OnInit {
     postExcerpt: new FormControl(""),
     postMetaTitle: new FormControl(""),
     postMetaDescription: new FormControl(""),
-    postFeatured: new FormControl("")
-
+    postFeatured: new FormControl(""),
   });
+
+  imagesUploadHandler(blobInfo, success, failure, progress) {
+    var xhr, formData;
+
+    xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
+    xhr.open("POST", "postAcceptor.php");
+
+    xhr.upload.onprogress = function (e) {
+      progress((e.loaded / e.total) * 100);
+    };
+
+    xhr.onload = function () {
+      var json;
+
+      if (xhr.status === 403) {
+        failure("HTTP Error: " + xhr.status, { remove: true });
+        return;
+      }
+
+      if (xhr.status < 200 || xhr.status >= 300) {
+        failure("HTTP Error: " + xhr.status);
+        return;
+      }
+
+      json = JSON.parse(xhr.responseText);
+
+      if (!json || typeof json.location != "string") {
+        failure("Invalid JSON: " + xhr.responseText);
+        return;
+      }
+
+      success(json.location);
+    };
+
+    xhr.onerror = function () {
+      failure(
+        "Image upload failed due to a XHR Transport error. Code: " + xhr.status
+      );
+    };
+
+    formData = new FormData();
+    formData.append("file", blobInfo.blob(), blobInfo.filename());
+
+    xhr.send(formData);
+  }
 
   scheduleSave(data: FormGroup) {
     clearTimeout(this.timeout);

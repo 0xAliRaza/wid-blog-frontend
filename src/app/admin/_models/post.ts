9cb2fae76f72ca3fb515d8ef450cce47abb1493a
Ali Raza <artanha7@gmail.com>
2021-01-13 12:06:04 +0500

    update `post` model

--- src/app/admin/_models/post.ts
+++ src/app/admin/_models/post.ts
@@ -1,17 +1,29 @@
-import { Tag } from './tag';
+import { Tag } from "./tag";
 import { User } from "./user";
 
-export interface Post {
+export class Post {
   id: number;
   user_id: number;
   title: string;
-  html: string;
-  featured_image: string;
+  html?: string;
+  featured_image?: string;
   slug: string;
   tags: Tag[];
-  custom_excerpt: string;
-  meta_title: string;
-  meta_description: string;
-  featured: boolean;
-  status: "draft" | "published";
+  custom_excerpt?: string;
+  meta_title?: string;
+  meta_description?: string;
+  featured?: boolean;
+  _published: boolean;
+  get published(): boolean {
+    return !!this._published;
+  }
+
+  set published(val: boolean) {
+    this._published = val;
+  }
+
+  get exists(): boolean {
+    return !!this.id;
+  }
+
 }

3074f32b673ef660e08b4c355ee4d512390e9c7c
Ali Raza <artanha7@gmail.com>
2021-01-24 17:51:31 +0500

    feat: Auto hide `appToggler.el` when clicked outside

--- src/app/toggler.directive.ts
+++ src/app/toggler.directive.ts
@@ -1,17 +1,38 @@
-import { Directive, ElementRef, HostListener } from "@angular/core";
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit,
+  Renderer2,
+  ViewChild,
+  ViewContainerRef,
+} from "@angular/core";
 
 @Directive({
   selector: "[appToggler]",
   exportAs: "appToggler",
 })
 export class TogglerDirective {
+  public el: ElementRef;
   public visible = false;
 
-  constructor() {}
+  constructor(private elementRef: ElementRef) {}
 
-  @HostListener("click")
-  onClick() {
-    this.toggle();
+  @Input() autoToggle = true;
+
+  @HostListener("document:click", ["$event"])
+  onDocumentClick(e) {
+    const targetElement = e.target as HTMLElement;
+    if (
+      targetElement &&
+      this.elementRef.nativeElement &&
+      this.elementRef.nativeElement.contains(targetElement)
+    ) {
+      this.toggle();
+    } else if (this.autoToggle) {
+      this.visible = false;
+    }
   }
 
   toggle() {

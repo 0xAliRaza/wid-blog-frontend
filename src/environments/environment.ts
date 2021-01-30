16e1b354487d288909e04061a892d5bc7add6341
Ali Raza <artanha7@gmail.com>
2021-01-30 06:37:03 +0500

    update `environment` according to new... ...containerized API endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const SERVER_URL = "http://wid-blog-backend";
+const SERVER_URL = "http://localhost:8000";
 
 export const environment = {
   production: false,
